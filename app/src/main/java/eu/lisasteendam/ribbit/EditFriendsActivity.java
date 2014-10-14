c0eb8248c7b1c6cf6f254a379323389ed6f91d5a
Lisa Steendam <lisa.m.m.steendam@gmail.com>
2014-10-14 09:53:56 +0200

    Dummy Content deleted

---
+++ app/src/main/java/eu/lisasteendam/ribbit/EditFriendsActivity.java
@@ -0,0 +1,44 @@
+package eu.lisasteendam.ribbit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+
+public class EditFriendsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_friends);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.orderByAscending("username");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.edit_friends, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+}
