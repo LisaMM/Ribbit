f55f61c9646ae4685ca7076e3412cb8db9f7918c
Lisa <lisa.m.m.steendam@gmail.com>
2014-10-23 11:29:11 +0200

    view image message

--- app/src/main/java/eu/lisasteendam/ribbit/ViewImageActivity.java
+++ app/src/main/java/eu/lisasteendam/ribbit/ViewImageActivity.java
@@ -3,9 +3,9 @@ package eu.lisasteendam.ribbit;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class ViewImageActivity extends Activity {
@@ -18,22 +18,7 @@ public class ViewImageActivity extends Activity {
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
         Uri imageUri = getIntent().getData();
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.view_image, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+        Picasso.with(this).load(imageUri.toString()).into(imageView);
     }
 }
